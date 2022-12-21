8cfa569f8b8b8ff7ec17969457879fcdfd59cd3c
Ali Gezik <bilgepanter@gmail.com>
2022-12-21 23:22:44 +0100

    update

---
+++ 30-Days-Of-JS/day-2/app.js
@@ -0,0 +1,64 @@
+// let sayi1 = '3' //numbers veri türü
+// let sayi2 = `3` //string veri türü
+
+
+// console.log(sayi1 === sayi2);
+
+// // console.log(numOne == numTwo)      // true ( Burada iki değişkende 3 e eşit olduğu için birbirine eşit olduğu sonucu çıkıyor )
+
+// let js = 'JavaScript'
+// let py = 'Python'
+
+// // console.log(js == py)             //false  ( Burada iki değişkende farklı olduğu ve eşitlenemedikleri için false yani 0 değeri dönüyor )
+
+// let lightOn = "true"
+// let lightOff = "true"
+
+// // console.log(lightOn == lightOff) 
+// // console.log(typeof lightOn);
+// // console.log(typeof lightOff);
+// console.log(typeof sayi1);
+// console.log(typeof sayi2);
+
+// let nums = {
+//     name:`mehmet`
+// }
+// let numbers = {
+//     name:`mehmet`
+// }
+
+// console.log(nums == numbers)
+// let age = 35
+
+// let mass = 72         // kilogram cinsinden kütle
+// const PI = 3.14 
+
+// console.log(age);
+
+const PI = Math.PI
+
+// console.log(PI);
+
+// console.log(Math.round(PI));
+
+// const gravity = 9.1  // yer çekimi kuvvet değeri bu değer haliyle değişmeyeceği için const olarak tanımlanıyor. Değişmez değişkenleri const ile tanımlarsınız. Bunu unutmayın.
+// console.log(Math.round(gravity)); //günlük
+// console.log(Math.floor(gravity)); //asagi
+// console.log(Math.ceil(gravity)); //yukari
+
+// console.log(Math.min(-5, 30, -20, 4,));
+
+const rn = Math.random()
+
+// console.log(Math.round(rn*100000));
+
+// console.log(Math.round(rn*100));
+
+// cons rn = 5
+
+console.log(Math.sqrt(100));
+
+console.log(100**(1/2));
+
+// Math.sin(0)
+console.log(Math.sin(53));
--- assignment-frontend/js/2-second/spp.js
+++ assignment-frontend/js/2-second/spp.js
@@ -12,12 +12,58 @@
 //! ODEV2: Console'dan girilen taban ve pozitif us degerlerine gore
 //! uslu sayi sonucunu hesaplayan kodu for veya while dongusu ile yaziniz.
 
-let taban = Number(prompt("taban sayisi giriniz"));
-let us = Number(prompt("us sayisi giriniz"));
+// let taban = Number(prompt("taban sayisi giriniz"));
+// let us = Number(prompt("us sayisi giriniz"));
 
-let sonuc = 1;
+// let sonuc = 1;
 
-for(i = 0; i < us; i++){
-        sonuc=sonuc*taban
+// for(i = 0; i < us; i++){
+//         sonuc=sonuc*taban
+// }
+// console.log(sonuc);
+
+// let grade = Number(prompt("your grade"));
+
+// while(grade < 0  || grade > 100){
+//         console.log("Grade should be 0-100")
+//         grade = Number(prompt("your grade: "))
+// }
+
+// console.log(`Your grade :${grade}`);
+
+// let counter = 0
+// let number = Number(prompt("enter a number"))
+
+// while(number != 0){
+//         number = Math.floor(number / 10)
+//         console.log(number);
+//         counter++
+// }
+// console.log(`Digit is count is : ${counter}`);
+
+// KREDI ORTALAMASI HESAPLIYORUZ SONRA TEKRAR NOT GIRMEK  ISTER MI DIYE SORUYORUZ
+
+
+// let again = " "
+// do{
+//         let avg = 0
+//         const midterm  = Number(prompt("your midterm :"))
+//         const final = Number(prompt("your final :"))
+//         avg = 0.4 * midterm + 0.6 * final;
+//         console.log(`Your score is ${avg}`);
+//         again = prompt("Do you want to continue e/E")
+
+// }while(again === "e" || again === "E");
+
+let result
+let line = ""
+
+for (let row = 1; row <= 10; row++){
+        for(let col = 1; col <= 10; col++){
+                result = row * col
+                line += result + "\t" // bir ta tusu ekle
+        }
+        line += "\n" // bir ta tusu ekle
 }
-console.log(sonuc);
+
+console.log(line);
