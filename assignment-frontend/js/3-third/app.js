53d00d18476186a8ae8a3bcf6ba2a7e8213e949a
Ali Gezik <bilgepanter@gmail.com>
2022-12-28 18:44:06 +0100

    q

--- assignment-frontend/js/3-third/app.js
+++ assignment-frontend/js/3-third/app.js
@@ -1,49 +1,66 @@
-// celcius to fahrenheit calculator#
+// // celcius to fahrenheit calculator#
 
-// function calFahrenheit(cel){
-//     const fahr =  cel * (9/5)+32
-//     return cel + ` derece `+ fahr + ` fahrenheita esittir`
-// }
+// // function calFahrenheit(cel){
+// //     const fahr =  cel * (9/5)+32
+// //     return cel + ` derece `+ fahr + ` fahrenheita esittir`
+// // }
 
-// console.log(calFahrenheit(30));
+// // console.log(calFahrenheit(30));
 
 
-//belirli bir string ifadenin tersini bulan fonksiyon olustur
+// //belirli bir string ifadenin tersini bulan fonksiyon olustur
 
-// let str = 'selamlar'
+// // let str = 'selamlar'
 
-// function reverseString(str) {
-//     let str2 = ''
-//     for (let i = str.length - 1; i >= 0; i--) {
-//         str2 += str[i]
+// // function reverseString(str) {
+// //     let str2 = ''
+// //     for (let i = str.length - 1; i >= 0; i--) {
+// //         str2 += str[i]
+// //     }
+// //     return str2
+// // }
+
+
+// // console.log(reverseString(`Hello world`));
+
+// // const string = `ali`
+
+// // function reverseString(str) {
+
+// function findVowels(str){
+//     let vowels = ''
+
+//     for(let i = 0; i < str.length; i++) {
+//         if(
+//             str[i] == 'a' ||
+//             str[i] == 'e' ||
+//             str[i] == 'i' ||
+//             str[i] == 'ı' ||
+//             str[i] == 'u' ||
+//             str[i] == 'ü' ||
+//             str[i] == 'o' ||
+//             str[i] == 'ö' ||
+//         ){
+//             vowels += str[i]
+//         }
 //     }
-//     return str2
+//     return vowels
 // }
+// console.log(findVowels('hello world'));
+
+let str1 = "hello world";
+let str2 = 'hello world';
+let str3 = `hello world`;
+let str4 = new String("Hello World");
 
 
-// console.log(reverseString(`Hello world`));
 
-// const string = `ali`
 
-// function reverseString(str) {
+console.log(str4.length);
+console.log(str4);
 
-function findVowels(str){
-    let vowels = ''
+let str = "Hello World"
 
-    for(let i = 0; i < str.length; i++) {
-        if(
-            str[i] == 'a' ||
-            str[i] == 'e' ||
-            str[i] == 'i' ||
-            str[i] == 'ı' ||
-            str[i] == 'u' ||
-            str[i] == 'ü' ||
-            str[i] == 'o' ||
-            str[i] == 'ö' ||
-        ){
-            vowels += str[i]
-        }
-    }
-    return vowels
+for(i= str.length-1; i>=; i--; ){
+    console.log(str[i]);
 }
-console.log(findVowels('hello world'));
