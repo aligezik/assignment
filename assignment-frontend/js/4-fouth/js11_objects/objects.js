6e199df85b1f748669e3a45d79d4a76ed4ee49fc
Ali Gezik <bilgepanter@gmail.com>
2023-01-07 12:15:19 +0100

    q

--- assignment-frontend/js/4-fouth/js11_objects/objects.js
+++ assignment-frontend/js/4-fouth/js11_objects/objects.js
@@ -10,3 +10,46 @@
 //? Herhangi bir veriyi erismek icin property (key) adi kullanilir.
 
 console.log("******* Objects *********")
+
+const car = new Object()
+car.brand = "Volvo"
+car.model = "1990"
+car.color = "agean blue"
+
+console.log(car);
+console.log(car.model); //? .notasyonu ile erisim
+console.log(car["model"]); //? square bracket ile erisim
+
+function Personel(id, ad, maas){
+    this.tc = id
+    this.name = ad
+    this.salary = maas
+}
+const p1 = new Personel("1234567890", "John", "40000")
+const p2 = new Personel("3338867890", "Clara", "34000")
+
+console.log(p1);
+console.log(p2);
+console.log(p1, p2);
+console.log(this);
+
+// window.alert("Merhaba")
+console.log(window.Personel);
+
+
+const worker = {
+    name: "Can",
+    surname: "Canan",
+    age: 33,
+    languages: ["C++", "Go", "Java", "JavaScript"],
+    salary: 10000
+}
+
+console.log(worker);
+console.log(worker.languages);
+
+worker["languages"].forEach((lang) => console.log(lang))
+
+// const upperCaseLangs = worker["languages"].map((lang) => 
+
+console.log(worker["name"]);
