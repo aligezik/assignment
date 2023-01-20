068d697145322e9fdc084ffbfbd2743db61690ba
Ali Gezik <bilgepanter@gmail.com>
2023-01-20 16:33:48 +0100

    qq

--- assignment-frontend/js/dom1/app.js
+++ assignment-frontend/js/dom1/app.js
@@ -4,5 +4,21 @@
 
 // document.querySelector(".list").nextElementSibling(".list");
 
+const newUl = document.querySelector("ul");
+
+// console.log(newUl);
+
 const newLi = document.createElement("li");
+newLi.setAttribute("id", "Steve"); //id atama
+newLi.setAttribute("class", "mentor"); //class atama
+
+const text = document.createTextNode("bilgepanter.com"); //icerik
+
+newLi.appendChild(text); //sona atiyor
+// newLi.prependChild(text); //basa atiyor
+
+newUl.appendChild(newLi);
+
+// newUl.prepend(newLi); //basa atiyor
+
 console.log(newLi);
