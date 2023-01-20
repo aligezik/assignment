db95729e5b4ee823749f8a3e6488bab23eb0d7e4
Ali Gezik <bilgepanter@gmail.com>
2023-01-20 17:39:36 +0100

    mouseover

--- assignment-frontend/js/dom1/app3.js
+++ assignment-frontend/js/dom1/app3.js
@@ -1,6 +1,20 @@
 
 
 
-document.querySelector("body")-addEventListener("mousemove", () => {
+document.querySelector("body").addEventListener("mousemove", (e) => {
+    // document.querySelector("body").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %255}, ${e.screenX %255 + e.screenY %255})`
     
+    document.querySelector(".container1").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %20}, ${e.screenX %255 + e.screenY %255})`
+    
+    document.querySelector(".container2").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %111}, ${e.screenX %200 + e.screenY %255})`
+    
+    document.querySelector(".container3").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %3}, ${e.screenX %150 + e.screenY %255})`
+    
+    document.querySelector(".container4").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %200}, ${e.screenX %100 + e.screenY %255})`
+    
+    
+
+
+
 })
+
