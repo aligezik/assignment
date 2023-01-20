d3e4d0aa3a8a4bebb05cb9394121b59ec47d77c6
Ali Gezik <bilgepanter@gmail.com>
2023-01-20 16:21:55 +0100

    dom

---
+++ assignment-frontend/js/dom1/app.js
@@ -0,0 +1,8 @@
+// const a = document.querySelector(".list");
+
+// const b = a.nextElementSibling(".list")
+
+// document.querySelector(".list").nextElementSibling(".list");
+
+const newLi = document.createElement("li");
+console.log(newLi);
--- assignment-frontend/playground/app.js
+++ assignment-frontend/playground/app.js
@@ -1,21 +1,31 @@
 function isOdd(number) {
-    return number % 2 != 0;
+  return number % 2 != 0;
 }
 
-function isEven(number){
-    return number % 2 == 0;
+function isEven(number) {
+  return number % 2 == 0;
 }
 
-function pickNumbers(numbers, fn){
-    let results = [];
-    for (const number of numbers){
-        if (fn(number)){
-            results.push(number);
-        }
+function pickNumbers(numbers, fn) {
+  let results = [];
+  for (const number of numbers) {
+    if (fn(number)) {
+      results.push(number);
     }
-    return results;
+  }
+  return results;
 }
 
-let numbers = [1, 2, 4, 7, 3, 5, 6 ];
+let numbers = [1, 2, 4, 7, 3, 5, 6];
 console.log(pickNumbers(numbers, isOdd));
 console.log(pickNumbers(numbers, isEven));
+
+const letters = ["A", "B", "C", , "E"];
+for (let i = 0; i < letters.length; i++) {
+  console.log(letters[i]);
+}
+
+const letters1 = ["A", "B", "C", , "E"];
+letters1.forEach((letters2) => {
+  console.log(letters2);
+});
