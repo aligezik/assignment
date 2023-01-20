9b72a9454a5e5c9a4c567e8bbcf27888cc5f34b8
Ali Gezik <bilgepanter@gmail.com>
2023-01-20 20:04:20 +0100

    q

--- assignment-frontend/js/dom1/app3.js
+++ assignment-frontend/js/dom1/app3.js
@@ -8,9 +8,9 @@ document.querySelector("body").addEventListener("mousemove", (e) => {
     
     document.querySelector(".container2").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %111}, ${e.screenX %200 + e.screenY %255})`
     
-    document.querySelector(".container3").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %3}, ${e.screenX %150 + e.screenY %255})`
+    document.querySelector(".container3").style.backgroundColor = `rgb(${e.screenX % 55}, ${e.screenY %3}, ${e.screenX %150 + e.screenY %255})`
     
-    document.querySelector(".container4").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %200}, ${e.screenX %100 + e.screenY %255})`
+    document.querySelector(".container4").style.backgroundColor = `rgb(${e.screenX % 255}, ${e.screenY %200}, ${e.screenX %100 + e.screenY %25})`
     
     
 
