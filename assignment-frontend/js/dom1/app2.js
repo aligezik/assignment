b55ee30fd1247a58d4ca1522d99cba037ffc956e
Ali Gezik <bilgepanter@gmail.com>
2023-01-20 17:20:01 +0100

    1

---
+++ assignment-frontend/js/dom1/app2.js
@@ -0,0 +1,9 @@
+// document.querySelector(".btn").addEventListener("click", () => {
+//   console.log("Clicked");
+// });
+
+document.querySelector(".btn").addEventListener("dblclick", () => {
+  console.log("Clicked1");
+});
+
+// mousedown, click***, dblclick, mouseup, mouseenter, mouseleave, mouseover, mouseout,  mousemove
---
+++ assignment-frontend/js/dom1/app3.js
@@ -0,0 +1,6 @@
+
+
+
+document.querySelector("body")-addEventListener("mousemove", () => {
+    
+})
