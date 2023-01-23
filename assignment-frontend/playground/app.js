106d47e886a2364c9f3e836ee79dfabae03d9436
Ali Gezik <bilgepanter@gmail.com>
2023-01-23 08:10:18 +0100

    q

--- assignment-frontend/playground/app.js
+++ assignment-frontend/playground/app.js
@@ -29,3 +29,25 @@ const letters1 = ["A", "B", "C", , "E"];
 letters1.forEach((letters2) => {
   console.log(letters2);
 });
+
+const salaries = [5500, 8000, 6500, 9000, 10000, 15000, 25000]
+
+// const moreThan10 = salaries.filter((s) => s >= 1000)
+const moreThan10 = salaries.filter((s) => s >= 10000 && s <= 20000 )
+// console.log(moreThan10);
+
+const az10 = salaries
+.filter((s) => s < 9000)
+.map((k) => Math.trunc(k * 1.1))
+.forEach((s) => console.log(s))
+
+// console.log(az10);
+
+console.log(salaries.reduce((s, x) => s * x, 1 ));
+console.log(salaries.reduce((s, x) => s + x, 0 )); //toplam zam
+
+const zamTop = salaries
+  .filter((s) => s < 9000)
+  .map((s) => Math.trunc(s * 1.1))
+  .reduce((s, t) => s + t, 0)
+console.log(`toplam yapilan zam`, zamTop);
