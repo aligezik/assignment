b1297b410eb5f54f09e97981c4846f1f300e32b3
Ali Gezik <bilgepanter@gmail.com>
2023-02-11 23:10:22 +0100

    q

--- assignment-frontend/js/2-second/spp.js
+++ assignment-frontend/js/2-second/spp.js
@@ -69,21 +69,68 @@
 // console.log(line);
 
 const cities = [
-        {name: 'Los Angeles', population: 3792621},
-        {name: 'Chicago', population: 2695598},
-        {name: 'New York', population: 8175133},
-        {name: 'Houston', population: 2099451},
-        {name: 'Philadelphia', population: 1526006}
-      ];
+        { name: 'Los Angeles', population: 3792621 },
+        { name: 'Chicago', population: 2695598 },
+        { name: 'New York', population: 8175133 },
+        { name: 'Houston', population: 2099451 },
+        { name: 'Philadelphia', population: 1526006 }
+];
 
 //       const x = cities.filter((cities => {
 //         return cities.population < 3000000;
 //       }))
 //       console.log(x);
 
-      //! filter sort map forEach
-      cities
-        .filter(ab => ab.population < 3000000)
-        .sort((a, b) => a.population - b.population)
-        .map((ab) => `${ab.name}:${ab.population}`)
-        .forEach((c) => console.log(c));
+//! filter sort map forEach
+//       cities
+//         .filter(ab => ab.population < 3000000)
+//         .sort((a, b) => a.population - b.population)
+//         .map((ab) => `${ab.name}:${ab.population}`)
+//         .forEach((c) => console.log(c));
+
+// What would be the output of the following three console.logs?
+
+// function withVar() { const b = () => a; var a = 24; return b; }
+
+// function withLet() { const b = () => a; let a = 24; return b; }
+
+// function changingValue() { let a = 24; const b = () => a; a = 42; return b; }
+
+// console.log(withVar()()); 
+// console.log(withLet()()); 
+// console.log(changingValue()()); 
+
+//* ==============================================================
+
+// const john = {
+//         firstName: 'John',
+//         lastName: `Doe`,
+//         birthDay: `1970`,
+//         fullName: function(){return `${this.firstName} ${this.lastName}`;},
+//         age(){return new Date().getFullYear()-this.birthDay;}
+// }
+
+// console.log(john);
+// console.log(john.firstName);
+// console.log(john.age());
+
+
+
+const bp = {
+        firstName: `Bilge`,
+        lastName: `Panter`,
+        birthDay: `2018`,
+        age() { return new Date().getFullYear() - this.birthDay },
+        fullName: function () { return `${this.firstName} ${this.lastName} is ${this.age()} years old`; },
+};
+console.log(bp.fullName());
+
+let aa = {
+        firstName: 'John',
+        lastName: 'Doe',
+}
+
+console.log(aa.firstName);
+
+const a1 = new Object();
+a1.
