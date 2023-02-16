0a943164c54d18e86fd32e0d7cc34a23213429a2
Ali Gezik <bilgepanter@gmail.com>
2023-02-16 13:09:09 +0100

    q

---
+++ assignment-frontend/react/personal/playground02/src/App.js
@@ -0,0 +1,14 @@
+import Msg from "./Msg"
+
+function App() {
+  return (
+    <div>
+      <h1>Hi React</h1>
+      <h3>React is a js library</h3>
+      <Msg name="John"/>
+      <Msg name="Bilge"/>
+    </div>
+  )
+}
+
+export default App
---
+++ assignment-frontend/react/personal/playground02/src/Msg.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Msg = (props) => {
+    console.log(props);
+  return <div>Hi {props.name}</div>
+}
+
+export default Msg
