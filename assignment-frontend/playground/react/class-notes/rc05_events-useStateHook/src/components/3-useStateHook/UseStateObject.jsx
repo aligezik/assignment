4272aa6fc3b2a95c1e1561ba96cf3a650891fc96
Ali Gezik <bilgepanter@gmail.com>
2023-03-26 22:00:26 +0200

    q

---
+++ assignment-frontend/playground/react/class-notes/rc05_events-useStateHook/src/components/3-useStateHook/UseStateObject.jsx
@@ -0,0 +1,114 @@
+// //* ==================== HOOKS ===============================
+// //! Hook'lar fonksiyonel component'ler icerisinde state'leri kullanmamiza
+// //! olanak saglayan ozel fonksiyonlardir.
+// //! React 16.8 versiyonu ile gelmistir ve geldikten sonra Class-componentler'in
+// //! kullanimi cok azaltmistir.
+
+// //? React'ta useState(), useEffect(), useContext() gibi bir cok built-in
+// //? Hook bulunmaktadir. Ayrica custom hook tanimlamak da mumkundur.
+
+// //* Hook Kullanim Kurallari:
+// //* 1. İlk olarak import edilmeliler. import { useState } from "react";
+// //* 2. Hook'lar ust seviyede kullanilmalidir. Yani Hook'lar bir
+// //*    dongunun, kosul cumleciginin ve icice fonksiyonlarin icerisinde
+// //*    kullanilmamalidir.
+// //* 3. Hook'lar sadece React Fonksiyonel componentleri icerisinde cagrilmalidir.
+// //*    Normal Javascript fonksiyonlari icerisinde cagrilmamalidir
+// //*    (Custom hook'lar icerisinde bir hook cagrilabilir)
+// //?    https://reactjs.org/docs/hooks-rules.html
+// //* =============================================================
+
+// import { useState } from "react"
+
+// const UseStateObject = () => {
+//   const [person, setPerson] = useState({
+//     name: "bilge",
+//     surname: "panter",
+//     salary: 10000,
+//   })
+
+//   const [toggle, setToggle] = useState(false)
+
+//   console.log(person)
+
+//   const handleSalaryInc = () => {
+//     setPerson({ ...person, salary: person.salary + 500 })
+//   }
+
+//   const handleSalaryDec = () => {
+//     setPerson({ ...person, salary: person.salary - 500 })
+//   }
+
+//   const handleNameToggle = () => {
+//     if (!toggle) {
+//       setPerson({
+//         ...person,
+//         name: person.name.toUpperCase(),
+//         surname: person.surname.toUpperCase(),
+//       })
+//     } else {
+//       setPerson({
+//         ...person,
+//         name: person.name.toLowerCase(),
+//         surname: person.surname.toLowerCase(),
+//       })
+//     }
+//     setToggle(!toggle)
+//   }
+
+//   return (
+//     <div className="container text-center mt-4">
+//       <h2 className="text-danger">Use State Object</h2>
+//       <h3 className="display-4">
+//         {person.name} {person.surname}
+//       </h3>
+//       <h3 className="display-6">Salary:{person.salary}</h3>
+//       <button onClick={handleSalaryInc} className="btn btn-success">
+//         Salary Inc
+//       </button>
+//       <button onClick={handleNameToggle} className="btn btn-danger">
+//         CLR
+//       </button>
+//       <button onClick={handleSalaryDec} className="btn btn-warning">
+//         Salary Dec
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default UseStateObject
+
+//!============================================================
+
+import { useState } from "react";
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({name: "Bilge", surname: "Panter", salary: 0});
+
+  const handleSalaryInc = () => {
+    setPerson({...person, salary: person.salary + 500})
+    // setPerson({name: person.name, surname: person.surname, salary: person.salary + 500})
+  }
+  const handleSalaryDec = () => {
+    setPerson({...person, salary: person.salary - 500})
+  }
+  const handleReset = () => {
+    setPerson({...person, name:"No Name", surname: person.surname.toUpperCase()})
+  }
+
+  return (
+    <div className="container text-center mt-4">
+      <h2 className="text-primary">Use State Object</h2>
+      <h1 className="display-4">
+        {person.name} {person.surname}
+        <br />
+        Salary:{person.salary} </h1>
+      <button onClick={handleSalaryInc} className="m-1 btn btn-success">Salary INC</button>
+      <button onClick={handleReset}className="m-1 btn btn-danger">Clear Name</button>
+      <button onClick={handleSalaryDec} className="m-1 btn btn-warning">Salary DEC</button>
+    </div>
+  );
+};
+
+export default UseStateObject;
+
