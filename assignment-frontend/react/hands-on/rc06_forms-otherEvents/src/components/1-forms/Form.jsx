ec9e52292c688df8018aa0bbba9851374499ebd4
Ali Gezik <bilgepanter@gmail.com>
2023-03-28 10:57:03 +0200

    q

--- assignment-frontend/react/hands-on/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ assignment-frontend/react/hands-on/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,5 +1,7 @@
 // import { useState } from "react"
 
+import { useState } from "react";
+
 // const Form = () => {
 //   const [username, setUsername] = useState("")
 //   const [email, setEmail] = useState("")
@@ -77,29 +79,47 @@
 
 // export default Form
 
-
-
 const Form = () => {
-  return (
-    <div>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  )
-}
 
-export default Form
+const [] = useState()
+
+
+  return (
+    <form>
+      <h2 className="display-6 text-danger">FORMS</h2>
+
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label">
+          User Name
+        </label>
+        <input type="email" className="form-control" id="username" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+        />
+      </div>
+      <button type="submit" className="btn btn-danger">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
