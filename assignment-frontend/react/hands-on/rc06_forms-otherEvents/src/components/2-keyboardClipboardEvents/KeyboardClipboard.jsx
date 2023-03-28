67ceb3496b6854a78b1d367de981c62d949c74e4
Ali Gezik <bilgepanter@gmail.com>
2023-03-28 14:45:50 +0200

    q

--- assignment-frontend/react/hands-on/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ assignment-frontend/react/hands-on/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,36 +1,37 @@
 // import { useState } from "react"
 
+
 // const KeyboardClipboard = () => {
-//   const [inputData, setInputData] = useState("")
-
-//   const handleKeyDown = (e) => {
-//     console.log(e.keyCode)
+    //   const [inputData, setInputData] = useState("")
+    
+    //   const handleKeyDown = (e) => {
+        //     console.log(e.keyCode)
 //     if (e.keyCode >= 48 && e.keyCode <= 57) {
-//       e.preventDefault() //? Rakam girisinin engellenmesi icin
-//       alert("Please dont enter a number")
-//     }
-//   }
-
-//   const handleAreaPaste = (e) => {
-//     console.log(e)
-//     e.target.style.fontSize = "30px"
-//     e.target.style.border = " 3px solid red"
-//     e.target.style.backgroundColor = "lightgrey"
-//     e.target.value += e.clipboardData.getData("text").toLocaleUpperCase()
-//     e.preventDefault()
-//   }
-
-//   const handleParCopy = (e) => {
-//     e.preventDefault()
-//     alert("you cannot copy this text")
+    //       e.preventDefault() //? Rakam girisinin engellenmesi icin
+    //       alert("Please dont enter a number")
+    //     }
+    //   }
+    
+    //   const handleAreaPaste = (e) => {
+        //     console.log(e)
+        //     e.target.style.fontSize = "30px"
+        //     e.target.style.border = " 3px solid red"
+        //     e.target.style.backgroundColor = "lightgrey"
+        //     e.target.value += e.clipboardData.getData("text").toLocaleUpperCase()
+        //     e.preventDefault()
+        //   }
+        
+        //   const handleParCopy = (e) => {
+            //     e.preventDefault()
+            //     alert("you cannot copy this text")
 //   }
 
 //   const handleParCut = (e) => {
-//     e.preventDefault()
-//     alert("you cannot cut this text")
-//   }
-
-//   return (
+    //     e.preventDefault()
+    //     alert("you cannot cut this text")
+    //   }
+    
+    //   return (
 //     <div className="container text-center">
 //       {/* <h2 className="display-5 text-danger">Keyboard-Clipboard Event</h2>
 
@@ -38,8 +39,8 @@
 //         type="text"
 //         className="form-control"
 //         onChange={(e) => {
-//           e.target.value = e.target.value.toLocaleUpperCase()
-//           setInputData(e.target.value)
+    //           e.target.value = e.target.value.toLocaleUpperCase()
+    //           setInputData(e.target.value)
 //         }}
 //         onKeyDown={handleKeyDown}
 //       />
@@ -65,11 +66,33 @@
 
 // export default KeyboardClipboard
 
+import { useState } from "react"
 
 const KeyboardClipboard = () => {
-  return (
-    <div>
-        <h2>KeyboardClipboard</h2>
+    
+    const [inputData, setInputData] = useState("")
+    
+    // const handleChange = () => {
+        
+    // }
+    
+    
+    
+    return (
+        <div>
+        <h2 className="display-5 text-danger">KeyboardClipboard</h2>
+
+        <input type="text" className="form-control" onChange={(e) => setInputData(e.target.value)} />
+
+        <p className="text-start mt-4">{inputData}</p>
+
+        <textarea 
+            className="form-control" 
+            name="textarea" 
+            id="textarea" 
+            cols="30" 
+            rows="10"
+        ></textarea>
     </div>
   )
 }
