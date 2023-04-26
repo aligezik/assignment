bcd7c26b13cc913d169f9192b586746efbfe368f
Ali Gezik <bilgepanter@gmail.com>
2023-04-26 22:02:29 +0200

    q

---
+++ assignment-backend/django/004_Python_Functions_Arguments_Modules/module_dir/my_module.py
@@ -0,0 +1,14 @@
+# Örnek modul
+
+def toplama(num1, num2):
+    '''
+    toplama islemini gerceklestirir
+    '''
+    return num1 + num2
+
+def carpma(num1, num2):
+    '''
+    carpma islemini gerceklestirir
+    '''
+    return num1 * num2
+
